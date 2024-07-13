d4325fe177e763f399983200105c2cbee7908b0d
RochakAd <rochakadhikari225@gmail.com>
2024-07-13 21:02:30 +0545

    pushing

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "imgui.h"
 #include "imgui-SFML.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
-#include "circuitElement.h"
-#include <SFML/Graphics.hpp>
-//#include "battery.h"
+
+
 using namespace sf;
 using namespace std;
- int batteryNumber = 0;
 
 
-const int numRows = 30;  
-const int numCols = 40;  
-const float cellSize = 20.0f;  // This is like pixels
-int col = 0;
-int row = 0;
+const int numRows = 30;
+const int numCols = 40;
+const float cellSize = 20.0f;
 
 struct Cell {
     RectangleShape shape;
 
-    Cell()  {
+    Cell() {
         shape.setSize(Vector2f(cellSize, cellSize));
         shape.setFillColor(Color::White);
         shape.setOutlineThickness(1.0f);
@@ -30,156 +28,149 @@ struct Cell {
     }
 };
 
- //2D vector
+
 vector<vector<Cell>> grid(numRows, vector<Cell>(numCols));
 
 class Draggable {
 protected:
-	bool isDragging;
-	Vector2f dragOffset;
+    bool isDragging;
+    Vector2f dragOffset;
 
 public:
-	RectangleShape shape;
-	Draggable(int width,int height, float x, float y)
-		: isDragging(false) {
-		shape.setSize(Vector2f(width,height));
-		shape.setPosition(x, y);
-	}
 
-	virtual void draw(RenderWindow& window) {
-		window.draw(shape);
-	}
+    RectangleShape shape;
 
-	Vector2f getPosition() {
-		return shape.getPosition();
-	}
-
-	void startDragging(Vector2f mousePosition) {
-		isDragging = true;
-		dragOffset = shape.getPosition() - mousePosition;
-	}
-	void stopDragging() {
-		isDragging = false;
-	}
-	void setColor(const Color& color) {
-		shape.setFillColor(color);
-	}
-	virtual void updatePosition(Vector2f mousePosition) {
-		Vector2f position;
-    if (row >= 0 && row < numRows && col >= 0 && col < numCols) {
-        float centerX = grid[row][col].shape.getPosition().x + grid[row][col].shape.getSize().x / 2.0f;
-		float centerY = grid[row][col].shape.getPosition().y + grid[row][col].shape.getSize().y;
-
-		position.x = centerX ;
-		position.y = centerY ;
+    Draggable(int width, int height, float x, float y)
+        : isDragging(false) {
+        shape.setSize(Vector2f(width, height));
+        shape.setPosition(x, y);
     }
 
-    if (isDragging) {
-        shape.setPosition(position + dragOffset);
+    void draw(RenderWindow& window) {
+        window.draw(shape);
+    }
+
+    Vector2f getPosition() {
+        return shape.getPosition();
+    }
+
+    void startDragging(Vector2f mousePosition) {
+        isDragging = true;
+        dragOffset = shape.getPosition() - mousePosition;
+    }
+
+    void stopDragging() {
+        isDragging = false;
+    }
+
+    void setColor(const Color& color) {
+        shape.setFillColor(color);
+    }
+
+    void updatePosition(Vector2f mousePosition) {
+        if (isDragging) {
+            shape.setPosition(mousePosition + dragOffset);
+        }
+    }
+};
+
+class CircuitElement {
+public:
+    bool is_connected = false;
+
+    virtual int connect() const = 0;
+};
+
+class Battery : public CircuitElement, public CircleShape {
+public:
+    int connect() const override {
+        return 1;
+    }
+
+};
+
+class Load : public CircuitElement {
+public:
+    void connect() {
+        is_connected = true;
+    }
+
+    int connect() const override {
+        return is_connected ? 1 : 0;
+    }
+};
+
+class Wire : public CircuitElement {
+public:
+    int connect() const override {
+        return 1;
+    }
+
+    int connectWith(const CircuitElement& element) const {
+        return element.is_connected ? 1 : 0;
+    }
+};
+
+
+class Switch : public CircuitElement {
+
+public:
+    bool is_switch_on = true;
+
+    int& toggle(int& state, bool click) {
+        if (!click) {
+            is_switch_on = false;
+            state = 0;
+        }
+        return state;
+    }
+
+    int connect() const override {
+        return is_switch_on ? 1 : 0;
+    }
+};
+
+
+struct Line {
+    Vertex points[2];
+
+
+    Line(Vector2f start, Vector2f end) {
+        points[0].position = start;
+        points[1].position = end;
+        points[0].color = Color::Black;
+        points[1].color = Color::Black;
+    }
+};
+
+
+void circuitConnection(bool switchToggle) {
+    int a, b, c;
+    unique_ptr<Battery[]> battery(new Battery[1]);
+    unique_ptr<Load[]> load(new Load[1]);
+    unique_ptr<Wire[]> wire(new Wire[2]);
+    unique_ptr<Switch[]> switch_toggle(new Switch[1]);
+
+    a = wire[0].connect() - battery[0].connect();
+    switch_toggle[0].toggle(a, switchToggle);
+    load[0].connect();
+    b = wire[1].connectWith(load[0]);
+    c = battery[0].connect() - b;
+
+    if (c == 1) {
+        cout << "The circuit is on" << endl;
+    }
+    else {
+        cout << "The circuit is off" << endl;
     }
 }
 
-};
-
-
-
-
-class Battery :public CircuitElement , public CircleShape
-{	
-public:
-	
-	int operator - (CircuitElement ce) {
-		return 1;
-	}
-};
-class Load :public CircuitElement
-{
-protected:
-
-public:
-
-	void connected(int x) {
-		if (x)
-		{
-			is_connected = true;
-		}
-
-	}
-
-	int operator - (CircuitElement ce) {
-		return 1;
-	}
-
-};
-class Wire :public CircuitElement
-{
-public:
-	int operator - (CircuitElement ce) {
-		return 1;
-	}
-	int operator + (CircuitElement ce) {
-		if (ce.is_connected) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-};
-class Switch :public CircuitElement, public RectangleShape
-{
-public:
-	bool is_switch_on = true;
-	int& toggle(int& a, bool click) {
-		if (click == false) {
-			is_switch_on = false;
-			a = 0;
-			return a;
-		}
-		else {
-			return a;
-		}
-
-
-	}
-};
-
-static void circuit_connection(bool switchToggle) {
-	int a, b, c,nb=1,nl=1,nw=2,ns=1;
-	Battery* battery;
-	Load* load;
-	Wire* wire;
-	Switch* switch_toggle;
-	switch_toggle = new Switch[ns];
-	wire = new Wire[nw];
-	load = new Load[nl];
-	battery = new Battery[batteryNumber];
-	
-	//Battery b1;
-	//Load l1;
-	//Wire w1, w2;
-	//Switch s1;
-	a = (*wire) - *(battery+0); // wire is connected to the battery
-	(*switch_toggle).toggle(a, switchToggle);
-	(*load).connected(a); // One end of load is connected to the wire
-	b = (*(wire+1)) + *load; //wire is connected to other end of the load
-	c = (*battery).connected(b);//the wire is connected to other end of battery
-	if (c == 1) {
-		//cout << "The circuit is on" << endl;
-	}
-	else {
-		//cout << "circuit is  off" << endl;
-	}
-
-}
-
-
-void handleHover(RenderWindow &window) {
+void handleHover(RenderWindow& window, int& row, int& col) {
     Vector2i mousePos = Mouse::getPosition(window);
     col = mousePos.x / cellSize;
     row = mousePos.y / cellSize;
+    cout << "Col: " << col << " Row: " << row << endl;
 
-	cout<<"Col: "<<col<<"Row: "<<row<<endl;
 }
 
 void initializeGrid() {
@@ -191,6 +182,7 @@ void initializeGrid() {
 }
 
 void drawGrid(RenderWindow& window) {
+
     for (int row = 0; row < numRows; ++row) {
         for (int col = 0; col < numCols; ++col) {
             window.draw(grid[row][col].shape);
@@ -198,148 +190,293 @@ void drawGrid(RenderWindow& window) {
     }
 }
 
-int main()
-{
-	bool lineOn = false;
-	bool once = true;
-	bool switchOn = true;
-	bool batteryadd = false;
-	Draggable Bulb(45,20, 200, 200);
+class Button {
+public:
+    Button() {} // Default constructor
 
-	//Test
-	Vertex TestLine[2];
-	TestLine[0].position =  Vector2f(0, 0);
-	TestLine[1].position =  Vector2f(0, 1);
-	RenderWindow window(VideoMode(numCols * cellSize, numRows * cellSize), "Simulation", Style::Close | Style::Resize);
-	initializeGrid();
-	//to draw a line
-	Vertex line[2];
-	line[0].position = Vector2f(0, 0);
-	Vector2f& refposition1 = line[0].position;
-	line[1].position = Vector2f(0, 0);
-	Vector2f& refposition2 = line[1].position;
-	//Vector2f& initialPositionOfLine = line[0].position;
-	//Vector2f& finalPositionOfLine = line[1].position;
-	line[0].color = Color::Black;
-	line[1].color = Color::Black;
-	//creating logic to add multiple batteries using <vector>
-	
+    Button(string text, Vector2f size, int charSize, Color bgColor, Color textColor) {
+        this->text.setString(text);
+        this->text.setFillColor(textColor);
+        this->text.setCharacterSize(charSize);
+
+        buttonShape.setSize(size);
+        buttonShape.setFillColor(bgColor);
+    }
+
+    void setFont(Font& font) {
+        text.setFont(font);
+    }
+
+    void setBackColor(Color color) {
+        buttonShape.setFillColor(color);
+    }
+
+    void setTextColor(Color color) {
+        text.setFillColor(color);
+    }
+
+    void setPosition(Vector2f pos) {
+       buttonShape.setPosition(pos);
+        // Center text within the button
+        float xPos = pos.x + (buttonShape.getSize().x - text.getLocalBounds().width) / 2;
+        float yPos = pos.y + (buttonShape.getSize().y - text.getLocalBounds().height) / 2 - text.getCharacterSize() / 4;
+        text.setPosition(xPos, yPos);
+    }
+
+    void drawTo(RenderWindow& window) {
+        window.draw(buttonShape);
+        window.draw(text);
+    }
+
+    bool isMouseOver(RenderWindow& window) {
+        Vector2f mousePos = window.mapPixelToCoords(Mouse::getPosition(window));
+        FloatRect bounds = buttonShape.getGlobalBounds();
+        return bounds.contains(mousePos);
+    }
+
+    bool isMouseClicked(RenderWindow& window) {
+        if (isMouseOver(window) && Mouse::isButtonPressed(Mouse::Left)) {
+            return true;
+        }
+        return false;
+    }
+
+private:
+    RectangleShape buttonShape;
+    Text text;
+};
+
+class MenuList {
+private:
+    bool isOpen; // Changed from int to bool to track open/close state
+    vector<Texture> textures;
+    vector<Sprite> sprites;
+    vector<ImTextureID> textureIDs;
+    int selectedItem; // Track selected item index
+
+public:
+    MenuList() : isOpen(false), selectedItem(-1) {} // Default constructor, menu is initially closed
+
+    void setTextures(const vector<Texture>& texs) {
+        textures = texs;
+
+        sprites.clear();
+        textureIDs.clear();
+
+        for (const auto& texture : textures) {
+            Sprite sprite;
+            sprite.setTexture(texture);
+            sprites.push_back(sprite);
+            textureIDs.push_back(reinterpret_cast<void*>(sprites.back().getTexture()->getNativeHandle()));
+        }
+    }
+
+    void drawMenu() {
+        if (isOpen) {
+            ImGui::Begin("Menu", &isOpen); // Pass the address of isOpen
+
+            ImGui::Columns(2, nullptr, false); // 2 columns, auto-width, no border
+
+            for (size_t i = 0; i < textureIDs.size(); ++i) {
+                ImGui::Image(textureIDs[i], ImVec2(100, 50)); // Fixed size of 150x100
+
+                if (ImGui::IsItemClicked()) {
+                    selectedItem = i;
+                }
+
+                ImGui::NextColumn(); // Move to next column
+            }
+
+            ImGui::End();
+        }
+    }
+
+    void toggle() {
+        isOpen = !isOpen;
+    }
+
+    bool isOpened() const {
+        return isOpen;
+    }
+
+    int getSelectedItemIndex() const {
+        return selectedItem;
+    }
+
+    Sprite& getSelectedSprite() {
+        if (selectedItem != -1) {
+            return sprites[selectedItem];
+        } else {
+             //returning a default sprite here if no sprite is loaded
+            static Sprite defaultSprite;
+            return defaultSprite;
+        }
+    }
+};
 
 
-	ImGui::SFML::Init(window);
-	vector<Battery>c;
+int main() {
+    int batteryNumber = 0;
+    bool lineOn = false;
+    bool switchOn = true;
+    bool batteryAdd = false;
 
-	Clock deltaClock;
-	while (window.isOpen()) {
-		Event event;
-		circuit_connection(switchOn);
-		
-		
+    Draggable Bulb(45, 20, 200, 200);
+    vector<Battery> batteries;
+    vector<Line> lines;
 
+    RenderWindow window(VideoMode(numCols * cellSize, numRows * cellSize), "Simulation", Style::Close | Style::Resize);
+    initializeGrid();
 
-		while (window.pollEvent(event)) {
-			ImGui::SFML::ProcessEvent(event);
-			if (event.type == Event::Closed) {
-				window.close();
-			}
-			else if (event.type == Event::MouseButtonPressed) {
-				if (event.mouseButton.button == Mouse::Left) {
-					Vector2f mousePosition(event.mouseButton.x, event.mouseButton.y);
-					// for (auto& resistor : resistors) {
-					if (Bulb.shape.getGlobalBounds().contains(mousePosition)) {
-						Bulb.startDragging(mousePosition);
-						// }
-					}
-				}
-				//to draw line
-				
+    ImGui::SFML::Init(window);
 
-				if (event.mouseButton.button == Mouse::Left && lineOn) {
-					Vector2f mousePosition(event.mouseButton.x, event.mouseButton.y);
-					//gets initial point to draw line
-					
-					Vector2f bulbPos(Bulb.getPosition().x, Bulb.getPosition().y);
-					refposition1 =
-						mousePosition;
-				}
+     
+     //button ko
+     Font font;
+    if (!font.loadFromFile("This Cafe.ttf")) {
+        return -1;
+    }
 
-				if (event.mouseButton.button == Mouse::Left && batteryadd) {
-					
-				
-					for (int i = 0; i < batteryNumber; i++) {
+     Button btn1("Icons", {200, 50}, 20, Color::Green, Color::Black);
+    btn1.setFont(font);
+    btn1.setPosition({10, 10});
 
-						c.push_back(Battery());
-						c.back().setRadius(20);
-						c.back().setFillColor(Color::Black);
-				}
-						c.back().setPosition(event.mouseButton.x, event.mouseButton.y);
-						
+    MenuList menu;
+    vector<Texture> textures;
+    vector<string> imagePaths = {
+        "textures/ResistorIcon.png",
+        "textures/CapacitorIcon.png",
+        "textures/InductorIcon.png",
+         "textures/ResistorIcon.png",
+        "textures/CapacitorIcon.png",
+        "textures/InductorIcon.png"
+    };
 
-				}
-				
-				
-				
-			}
-			else if (event.type == Event::MouseButtonReleased) {
-				if (event.mouseButton.button == Mouse::Left) {
-					// for (auto& resistor : resistors) {
-					Bulb.stopDragging();
-					// }
-				}
-				if (event.mouseButton.button == Mouse::Left && lineOn) {
-					//to get final position for the line
-					Vector2f mousePosition(event.mouseButton.x, event.mouseButton.y);
-					refposition2 = mousePosition;
-				}
-			}
-			
-		}
-		if (Mouse::isButtonPressed(Mouse::Left)) {
-			Vector2f mousePos(Mouse::getPosition(window));
-			// for (auto& resistor : resistors) {
-			Bulb.updatePosition(mousePos);
-			// }
-		}
+    // Load textures
+    for (const auto& path : imagePaths) {
+        Texture texture;
+        if (texture.loadFromFile(path)) {
+            textures.push_back(texture);
+        } else {
+            cout << "Error in loading image: " << path << endl;
+        }
+    }
 
-		ImGui::SFML::Update(window, deltaClock.restart());
-		ImGui::Checkbox("switch", &switchOn);
-		if (ImGui::Button("Battery")) {
-			cout << "added battery"<<endl;
-			batteryadd = !batteryadd;
-			batteryNumber++;
-			cout << batteryNumber << endl;
-		}
-		if (ImGui::Button("Line"))
-		{
-			lineOn = !lineOn;
-			cout << lineOn;
-		}
-
-		window.clear(Color::Black);
-		drawGrid(window);
+    // Set textures for menu
+    menu.setTextures(textures);
 
 
-		// Bulb.setRadius(20);
-		// Bulb.setPosition(200, 200);
-		if (switchOn) {
+    Clock deltaClock;
 
-			Bulb.setColor(Color::Red);
-		}
-		else {
-			Bulb.setColor(Color::Black);
+    while (window.isOpen()) {
+        Event event;
+        circuitConnection(switchOn);
 
-		}
-		for (int i = 0; i < c.size(); i++) {
-			window.draw(c[i]);
-		}
-		window.draw(TestLine,2,Lines);
-		window.draw(line,2,Lines);
-		ImGui::SFML::Render(window);
-		handleHover(window);
-		Bulb.draw(window);
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            else if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    Vector2f mousePosition(event.mouseButton.x, event.mouseButton.y);
+                    if (Bulb.shape.getGlobalBounds().contains(mousePosition)) {
+                        Bulb.startDragging(mousePosition);
+                    }
 
-		window.display();
-	}
-	ImGui::SFML::Shutdown();
-	return 0;
+                    if (lineOn) {
+                        lines.push_back(Line(mousePosition, mousePosition));
+                    }
+
+                    if (batteryAdd) {
+                        batteries.push_back(Battery());
+                        batteries.back().setRadius(20);
+                        batteries.back().setFillColor(Color::Black);
+                        batteries.back().setPosition(mousePosition);
+                        batteryAdd = false;
+                    }
+                }
+            }
+            else if (event.type == Event::MouseButtonReleased) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    Bulb.stopDragging();
+                    if (lineOn) {
+                        lines.back().points[1].position = Vector2f(event.mouseButton.x, event.mouseButton.y);
+                        lineOn = false;
+                    }
+                }
+            }
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Left)) {
+            Vector2f mousePos(Mouse::getPosition(window));
+            Bulb.updatePosition(mousePos);
+
+            if (lineOn && !lines.empty()) {
+                lines.back().points[1].position = mousePos;
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::Checkbox("Switch", &switchOn);
+        if (ImGui::Button("Add Battery")) {
+            batteryAdd = true;
+            batteryNumber++;
+        }
+        if (ImGui::Button("Draw Line")) {
+            lineOn = true;
+        }
+
+        //button ko
+          if (btn1.isMouseOver(window)) {
+            btn1.setBackColor(Color::White);
+        } else {
+            btn1.setBackColor(Color::Green);
+        }
+
+        if (btn1.isMouseClicked(window)) {
+            cout << "Button clicked!" << endl;
+            menu.toggle(); // Toggle menu visibility
+        }
+
+
+        window.clear(Color::Black);
+        drawGrid(window);
+
+        
+
+        if (switchOn) {
+            Bulb.setColor(Color::Red);
+        }
+        else {
+            Bulb.setColor(Color::Black);
+        }
+
+        for (auto& battery : batteries) {
+            window.draw(battery);
+        }
+
+        for (auto& line : lines) {
+            window.draw(line.points, 2, Lines);
+        }
+
+        btn1.drawTo(window);
+        menu.drawMenu();
+
+        
+        // Render selected item's image at the center of the window
+       Sprite& selectedSprite = menu.getSelectedSprite();
+       selectedSprite.setScale(50.0f / selectedSprite.getLocalBounds().width,  35.0f / selectedSprite.getLocalBounds().height);
+       selectedSprite.setPosition(window.getSize().x / 2 - 25,window.getSize().y / 2 - 17.5f); //randomass size
+        window.draw(selectedSprite);
+
+        ImGui::SFML::Render(window);
+        int row = 0, col = 0;
+        handleHover(window, row, col);
+        Bulb.draw(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    return 0;
 }
